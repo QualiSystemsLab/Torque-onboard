839f652d974b59331d32e9509efb9760069e56dd
Chris530906 <73956778+Chris530906@users.noreply.github.com>
2021-12-15 11:54:48 -0800

    Update healthcheck.sh

--- applications/promotions-manager/healthcheck.sh
+++ applications/promotions-manager/healthcheck.sh
@@ -9,6 +9,7 @@ do
     sleep 1
     if [[ $(wget -S -T1 -t1 http://${hostname}/ 2>&1 | grep "HTTP/1" | awk '{print $2}') == "200" ]] ; then
         echo 'connected to' ${hostname}
+        exit 1
         exit 0
     else
         echo 'cannot connect to' ${hostname}
